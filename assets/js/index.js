35919cdb3dba0d1a9598c35ec9b82be170f86c9b
Amy <bergstromaa@gmail.com>
2024-07-18 20:48:46 +0100

    update landing page, update animations on index page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,18 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('.slide-in');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            }
+        });
+    }, { threshold: 0.1 });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});

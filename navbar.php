8565681302c1aaf58160497590746c828b5089a5
Amy <bergstromaa@gmail.com>
2025-08-13 10:59:13 +0100

    add study club page and update navbar

--- navbar.php
+++ navbar.php
@@ -1,11 +1,10 @@
-<!-- Navigation -->
 <nav class="navbar navbar-expand-lg fixed-top">
   <div class="container">
     <a class="navbar-brand d-flex align-items-center" href="index.php">
       <img src="./assets/images/logo-red.png" height="40" alt="Maths tutoring with Amy logo" class="me-2" />
       <span class="logo-words d-none d-sm-block">Maths Tutoring with Amy</span>
     </a>
-   <a href="contact.php" class="btn btn-primary d-lg-none">Message Me</a>
+    <a href="contact.php" class="btn btn-primary d-lg-none">Message Me</a>
 
     <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
       <span class="navbar-toggler-icon"></span>
@@ -23,21 +22,26 @@
             <li><a class="dropdown-item" href="framework-values.php">Framework and Values</a></li>
           </ul>
         </li>
+
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
             aria-expanded="false">Tutoring Services</a>
           <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="./GCSE.php">GCSE/IGCSE Tutoring</a></li>
-            <li><a class="dropdown-item" href="./recovery.php">12 Week Intensive A Level Recovery
-                Programme</a></li>
+            <li><h6 class="dropdown-header">GCSE</h6></li>
+            <li><a class="dropdown-item" href="./GCSE.php">GCSE/IGCSE Tutoring</a></li>
             <li><a class="dropdown-item" href="./info-pack.php">Tutoring Information Pack</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><h6 class="dropdown-header">A Level</h6></li>
+            <li><a class="dropdown-item" href="./study-club.php">*NEW* A Level Maths Study Club</a></li>
+            <li><a class="dropdown-item" href="./recovery.php">12 Week Intensive A Level Recovery Programme</a></li>
           </ul>
         </li>
+        
         <li class="nav-item">
           <a class="nav-link" href="contact.php">Contact</a>
         </li>
       </ul>
-         <a href="contact.php" class="btn btn-primary d-none d-lg-block">Message Me</a>
+      <a href="contact.php" class="btn btn-primary d-none d-lg-block">Message Me</a>
     </div>
   </div>
 </nav>

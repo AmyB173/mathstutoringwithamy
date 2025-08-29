3933f9fcfd9304824c9b82a281b24bb6655478cf
Amy <bergstromaa@gmail.com>
2025-08-29 12:21:46 +0100

    update css file path

--- study-club.php
+++ study-club.php
@@ -23,7 +23,7 @@
     <link rel="canonical" href="https://www.mathstutoringwithamy.co.uk/study-hub.php" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
 
-    <link href="/assets/css/style-new.css" rel="stylesheet" />
+    <link href="./assets/css/style-new.css" rel="stylesheet" />
 
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
